a056f96289480697da4406246665d2a3626e62ea
Jerryomwesh <jerryomwesh@gmail.com>
2025-08-04 17:06:53 +0300

    while, for, loops

---
+++ loop.py
@@ -0,0 +1,20 @@
+#for , while, do_while
+
+# while loop
+to_loop =True
+i=0
+while to_loop:
+    if i>10:
+      to_loop =False
+    print ("Value is", i)
+    i=i+1
+
+k=0
+while k<2:
+    print("k is", k)
+    k+= 1
+
+
+ #for loop   starting condition, ending condition, increment
+for i in range (0,2):
+    print("for loop i is", i)
