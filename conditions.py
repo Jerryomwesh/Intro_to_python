af172dd94b00bb20065fb35b95d9bcccf6cfa73e
Jerryomwesh <jerryomwesh@gmail.com>
2025-08-04 17:06:02 +0300

    while, for loop

---
+++ conditions.py
@@ -0,0 +1,16 @@
+# age =20
+
+# if age>20:
+#     print("You can drink")
+
+# else:
+#     print("You are under age")
+
+age =21
+
+if age >20:
+    print("This is the legal age to drink")
+else: 
+    print ("You are under age")
+
+     
