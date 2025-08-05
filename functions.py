fd743cbe7eb25a2b68a18c7e36ad73e029e7aa42
Jerryomwesh <jerryomwesh@gmail.com>
2025-08-05 09:44:27 +0300

    initial commit

---
+++ functions.py
@@ -0,0 +1,12 @@
+
+
+
+def greet (name="Jerry", greeting="Hello", country="kenya"):
+        print (greeting, name, "welcome to", country)
+
+greet("Jerry", "Kenya")
+
+# def greet (greeting, name):
+#     print (greeting, name)
+
+# greet ("Hello", "Jerry")
