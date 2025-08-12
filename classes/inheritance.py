92fadb9de31195582f304816a72ff601d77f8ea7
Jerryomwesh <jerryomwesh@gmail.com>
2025-08-12 09:30:12 +0300

    inheritance intro

--- classes/inheritance.py
+++ classes/inheritance.py
@@ -1,10 +1,29 @@
 
 class Shape:
-    def __init__(self, name):
+    def __init__(self, name,side):
         self.name = name
+        self.side = side
 
     def describe (self):
         print(f"This is a {self.name}")
 
-shape1=Shape(name="Circle")
-shape1.describe()            
+#shape1=Shape(name="Circle")
+#shape1.describe() 
+
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        super().__init__("Rectangle", 4)
+        self.length = length
+        self.width = width
+
+    def area(self):
+        a=self.width*self.length
+        print(f"For {self.name} area is {a}")
+        return self.length * self.width
+
+
+r1=Rectangle(3,4)
+r1.describe()
+r1.area()
+
---
+++ classes/inheritance2.py
@@ -0,0 +1,14 @@
+class User():
+    def__init_(self,name,email,phone,address)
+        self.name=name
+        self.email=email
+        self.phone=phone
+        self.address=address
+
+
+    def say_my_name(self):
+        print(f"My name is {self.name}")       
+
+
+
+        
